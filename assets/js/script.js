b5ace069a3fad6ead11ff54331351345041a245f
Arthur Vilela <arthur.vilela@gmail.com>
2024-07-21 17:59:50 +0000

    Add comments on JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,29 @@ const startGameBtn = document.getElementById("start-game-btn");
 const checkScoreBtn = document.getElementById("check-score-btn")
 const restartBtn = document.getElementById("restart");
 
-
+/**
+ * This function will hide the home screen and
+ * display the initial game screen
+ */
 function startNewGame() {
     homePageDiv.style.display = "none";
     gamePageDiv.style.display = "block";
 
 }
 
+/**
+ * This function will hide the game screen and
+ * display the final score screen
+ */
 function checkScore() {
     gamePageDiv.style.display = "none";
     scorePageDiv.style.display = "block"
 }
 
+/**
+ * This function will hide the game screen and
+ * display the home screen
+ */
 function restartGame() {
     scorePageDiv.style.display = "none";
     homePageDiv.style.display = "block";
